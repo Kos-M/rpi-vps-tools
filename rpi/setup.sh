921d47efb07b1e57e6f2ad8b04e2190f59dec954
KosM <mpoukouvalas.kk@gmail.com>
2022-02-25 23:32:10 +0200

    change some typos , added default platform install location

--- rpi/setup.sh
+++ rpi/setup.sh
@@ -2,9 +2,13 @@
 
 source ./functions.sh
 if [ ! -f "./.env" ] ; then log ".env not found abort"; exit ; fi
-source .env
+source ./.env
 
 log "Starting setup.."
+if [ ! -d "$PLATFORMS_LOCATION" ] ; then 
+    log "Creating platform locations directory.."
+    mkdir -- "$PLATFORMS_LOCATION"
+fi
 
 if [ "$SYSTEM_UPGRADE" == "Y" ] ; then
     log "Upgrading the system.."
@@ -25,24 +29,20 @@ fi
 if [ "$NODE" == "Y" ] ; then
     log "Installing NodeJS $NODE_VERSION"
     install_node "$NODE_VERSION"
-    #
 fi
 if [ "$PIHOLE" == "Y" ] ; then
     log "Installing pihole network wide ad-blocker.."
-    #
+    install_pihole
 fi
 if [ "$HOSTAPD" == "Y" ] ; then
     log "Installing hostapd hotspot.."
-    #
+    install_hostapd
 fi
 if [  "$GITEA" == "Y" ] ; then
     log "Installing gitea.."
-    #
+    install_gitea
 fi
 if [ "$DOCKER" == "Y" ] ; then
     log "Installing docker and docker-compose.."
     install_docker
 fi
-# if [ "$GITEA" == "Y" ] ; then
-#     #
-# fi
